b2bd3bfb18ae4c3358e76b012b466ee01aaf7bc5
Hrithvik Aryan Singh <singhhrithvikaryan@gmail.com>
2025-07-20 10:59:43 +0530

    Add Buy and Sell routes back to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+// Frontend/src/App.jsx
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Components/Home.jsx';
 import About from './pages/About.jsx';
 import BuyItems from './pages/BuyItems.jsx';
 import ItemDetails from './pages/ItemDetails.jsx';
-import SellItem from './pages/SellItem.jsx'; 
+import SellItem from './pages/SellItem.jsx';
+import Login from './pages/Login.jsx';
+import ContactUs from './pages/ContactUs.jsx';
 
 function App() {
   return (
@@ -14,8 +17,10 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/buy" element={<BuyItems />} />
-          <Route path="/sell" element={<SellItem />} /> 
+          <Route path="/sell" element={<SellItem />} />
           <Route path="/item/:id" element={<ItemDetails />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/contact" element={<ContactUs />} />
         </Routes>
       </main>
     </div>
