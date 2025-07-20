f374797c9a34532022efe59063da8537cfe77b82
Hrithvik Aryan Singh <singhhrithvikaryan@gmail.com>
2025-07-20 10:29:43 +0530

    Add SellItem feature with routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-// Frontend/src/App.jsx
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Components/Home.jsx';
 import About from './pages/About.jsx';
-import BuyItems from './pages/BuyItems.jsx';         
-import ItemDetails from './pages/ItemDetails.jsx';   
+import BuyItems from './pages/BuyItems.jsx';
+import ItemDetails from './pages/ItemDetails.jsx';
+import SellItem from './pages/SellItem.jsx'; 
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/buy" element={<BuyItems />} />
+          <Route path="/sell" element={<SellItem />} /> 
           <Route path="/item/:id" element={<ItemDetails />} />
         </Routes>
       </main>
