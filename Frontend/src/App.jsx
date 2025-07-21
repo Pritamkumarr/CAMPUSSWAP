81dcd68f314d21c189b1962f49ccf139c4c76ebd
@pritam11092006 <pritam11092006@gmail.com>
2025-07-21 15:20:37 +0530

    resolved merge conflicts

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,11 +3,16 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Components/Home.jsx';
 import About from './pages/About.jsx';
+import Login from './pages/Login.jsx';
+import ContactUs from './pages/ContactUs.jsx';
+import Signup from './pages/Signup.jsx';
+import PrivacyPolicy from './pages/PrivacyPolicy.jsx';
+import TermsOfService from './pages/TermsOfService.jsx';
+import Faq from './pages/faq.jsx';
 import BuyItems from './pages/BuyItems.jsx';
 import ItemDetails from './pages/ItemDetails.jsx';
 import SellItem from './pages/SellItem.jsx';
-import Login from './pages/Login.jsx';
-import ContactUs from './pages/ContactUs.jsx';
+
 
 function App() {
   return (
@@ -16,11 +21,15 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/contact" element={<ContactUs />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/privacy" element={<PrivacyPolicy />} />
+          <Route path="/terms" element={<TermsOfService />} />
+          <Route path="/faq" element={<Faq />} />
           <Route path="/buy" element={<BuyItems />} />
           <Route path="/sell" element={<SellItem />} />
           <Route path="/item/:id" element={<ItemDetails />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/contact" element={<ContactUs />} />
         </Routes>
       </main>
     </div>
